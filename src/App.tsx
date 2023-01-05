d7eec72ed43d212e698596a3813513b8e666ccca
hilu <nikanozadze1010@gmail.com>
2023-01-06 02:38:18 +0400

    feat: work with router and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { lazy, Suspense } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("pages/Home"));
+const About = lazy(() => import("pages/About"));
+const Subscribe = lazy(() => import("pages/Subscribe"));
+
 function App() {
-  return <div className="w-screen"></div>;
+  return (
+    <div className="w-screen">
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route
+          path="/home"
+          element={
+            <Suspense>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Suspense>
+              <About />
+            </Suspense>
+          }
+        />
+        <Route
+          path="subscribe"
+          element={
+            <Suspense>
+              <Subscribe />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
