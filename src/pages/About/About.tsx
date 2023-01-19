10ae3416fd2ec1c95a8894c62ef24322bad60f3c
hilu <nikanozadze1010@gmail.com>
2023-01-19 10:55:23 +0400

    wip: work with about page markup

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,22 @@
+import { Commitment } from "../../components";
+
 const About = () => {
-  return <div>About us</div>;
+  return (
+    <div className="w-full mt-[90px] px-6 md:mt-[119px] md:px-10 xl:px-20">
+      <div className="w-full h-[400px] rounded-[10px] bg-no-repeat bg-center bg-cover px-6 flex flex-col items-center about-poster-mobile">
+        <h1 className="fraunces font-black text-[28px] leading-[28px] text-light mt-[111px]">
+          About Us
+        </h1>
+        <p className="mt-6 barlow font-normal text-[15px] leading-[25px] text-light opacity-80 text-center md:w-96 md:text-left">
+          Coffeeroasters began its journey of exotic discovery in 1999,
+          highlighting stories of coffee from around the world. We have since
+          been dedicated to bring the perfect cup - from bean to brew - in every
+          shipment.
+        </p>
+      </div>
+      <Commitment />
+    </div>
+  );
 };
 
 export default About;
